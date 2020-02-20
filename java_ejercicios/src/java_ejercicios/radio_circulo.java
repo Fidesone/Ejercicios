4d0baf24f52b3367b6b2f6481e59156d5b14ef2b
HGonz <HGonz@10.118.89.70>
2020-02-20 18:14:04 +0100

    Calcular el radio de un círculo

---
+++ java_ejercicios/src/java_ejercicios/radio_circulo.java
@@ -0,0 +1,24 @@
+package java_ejercicios;
+
+import java.util.Scanner;
+
+
+
+public class radio_circulo {
+
+	public static void main(String[] args) {
+		// 2. Pedir el radio de un círculo y calcular su área. A=PI*r^2.
+		
+		Scanner scanner= new Scanner(System.in);
+		
+		System.out.println("Introduce el radio");
+		double radio =  (double) scanner.nextInt();
+		
+		double area = Math.PI * (radio* radio);
+		
+		System.out.println("El área del círculo es: "+ area);
+		
+		
+	}
+
+}
